8352f32a39cbe2618f7e13d21c2982afa68c5364
KaustavSaikia <kaustavmsaikia@gmail.com>
2017-06-04 22:32:59 +0530

    Update typing text

--- js/scripts.js
+++ js/scripts.js
@@ -98,7 +98,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Software QA Professional.", "I Love Developing Automation Framework.", "I Live in Bangalore, India."],    
+            strings: ["I am A Software QA Professional.", "Love to Automate", "I Live in Bangalore, India."],    
 			/* You can change the home section typing text from here and do not use "&" use "and" */
             typeSpeed: 50
           });
